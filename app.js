271edc647d6c05a563b7d21a65cd3751ba541a2e
xiaobaifeng <835277213@qq.com>
2019-10-12 14:45:31 +0800

    feat: 完成koa-router的例子;

--- app.js
+++ app.js
@@ -1,34 +1,31 @@
 const Koa = require('koa')
+// 注意 require('koa-router')返回的是函数:
+const router = require('koa-router')();
 const app = new Koa()
 
 const address = require('address')
 const opn = require('opn')
 
-app.use(async (ctx, next) => {
-	let stime = new Date().getTime()
-	await next()
-	let etime = new Date().getTime()
-	ctx.response.type = 'text/html'
-	ctx.response.body = '<h1>Hello World</h1>'
-	console.log(`请求地址：${ctx.path}，响应时间：${etime - stime}ms`)
+// 为所有请求设置跨域头 *: 通配符
+router.all('/*', async (ctx, next) => {
+	// *代表允许来自所有域名请求
+	ctx.set("Access-Control-Allow", "*");
+	// 其他一些设置
+	await next();
+})
+// 添加路由
+router.get('/', async (ctx, next) => {
+	ctx.response.body = `<h1>Index page</h1>`
+})
+router.get('/home', async (ctx, next) => {
+	ctx.response.body = `<h1>Home page</h1>`
+})
+router.get('/404', async (ctx, next) => {
+	ctx.response.body = `<h1>404 page</h1>`
 })
 
-app.use(async (ctx, next) => {
-	console.log('中间件1 doSomething')
-	await next()
-	console.log('中间件1 end')
-})
-
-app.use(async (ctx, next) => {
-	console.log('中间件2 doSomething')
-	console.log('中间件2 end')
-})
-
-app.use(async (ctx, next) => {
-	console.log('中间件3 doSomething')
-	await next()
-	console.log('中间件3 end')
-})
+// 调用路由中间件
+app.use(router.routes())
 
 app.listen(3000, () => {
 	let localhost = address.ip();
