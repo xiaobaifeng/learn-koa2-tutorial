bbb983b4cf4bcd03ec6ca07bd0cb693f811ecbf2
xiaobaifeng <835277213@qq.com>
2019-10-12 12:41:59 +0800

    feat: 项目启动时，默认在浏览器打开访问项目url;

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const Koa = require('koa')
 const app = new Koa()
 
+const address = require('address')
+const opn = require('opn')
+
 app.use(async (ctx, next) => {
 	await next()
 	ctx.response.type = 'text/html'
@@ -8,5 +11,8 @@ app.use(async (ctx, next) => {
 })
 
 app.listen(3000, () => {
-	console.log('server is running at ', '\033[36;4mhttp://localhost:3000\033[36;4m')
+	let localhost = address.ip();
+	let url = 'http://' + (localhost || 'localhost') + ':' + '3000'
+	console.log('server is running at ', '\033[36;4m' + url + '\033[0m')
+	opn(url)
 })
