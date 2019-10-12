c5f2a5c99fe7effef634eaadc8390494f6fa176a
xiaobaifeng <835277213@qq.com>
2019-10-12 16:56:24 +0800

    refactor: 分离controller层;

--- router.js
+++ router.js
@@ -1,5 +1,6 @@
 // 注意 require('koa-router')返回的是函数:
 const router = require('koa-router')()
+const HomeController = require('./controller/home')
 
 module.exports = (app) => {
     
@@ -11,44 +12,11 @@ module.exports = (app) => {
             await next();
         }) 
     */
-    
-    router.get('/', async(ctx, next) => {
-        ctx.response.body = `<h1>index page</h1>`
-    })
-
-    router.get('/home', async(ctx, next) => {
-        console.log(ctx.request.query)
-        console.log(ctx.request.querystring)
-        ctx.response.body = '<h1>HOME page</h1>'
-    })
-  
-    router.get('/home/:id/:name', async(ctx, next)=>{
-        console.log(ctx.params)
-        ctx.response.body = '<h1>HOME page /:id/:name</h1>'
-    })
-  
-    router.get('/user', async(ctx, next)=>{
-        ctx.response.body = 
-        `
-        <form action="/user/register" method="post">
-            <input name="name" type="text" placeholder="请输入用户名：ikcamp"/> 
-            <br/>
-            <input name="password" type="text" placeholder="请输入密码：123456"/>
-            <br/> 
-            <button>GoGoGo</button>
-        </form>
-        `
-    })
-  
-    // 增加响应表单请求的路由
-    router.post('/user/register',async(ctx, next)=>{
-        let {name, password} = ctx.request.body
-        if( name == 'ikcamp' && password == '123456' ){
-            ctx.response.body = `Hello， ${name}！` 
-        }else{
-            ctx.response.body = '账号信息错误'
-        }
-    })
+    router.get('/', HomeController.index)
+    router.get('/home', HomeController.home)
+    router.get('/home/:id/:name', HomeController.homeParams)
+    router.get('/user', HomeController.use)
+    router.post('/user/register', HomeController.register)
   
     // 调用路由中间件
     app.use(router.routes())
