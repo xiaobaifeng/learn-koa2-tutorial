bc297064b45716c19ce6d0adaead4d555aa0ebe1
xiaobaifeng <835277213@qq.com>
2019-10-12 15:10:50 +0800

    refactor: 重构app.listen;

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const Koa = require('koa')
 const app = new Koa()
-
-const address = require('address')
-const opn = require('opn')
+const listener = require('./listener')
 
 app.use(async (ctx, next) => {
 	await next()
@@ -10,9 +8,4 @@ app.use(async (ctx, next) => {
 	ctx.response.body = '<h1>Hello World</h1>'
 })
 
-app.listen(3000, () => {
-	let localhost = address.ip();
-	let url = 'http://' + (localhost || 'localhost') + ':' + '3000'
-	console.log('server is running at ', '\033[36;4m' + url + '\033[0m')
-	opn(url)
-})
+listener(app);
