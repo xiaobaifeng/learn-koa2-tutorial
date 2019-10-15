8f3815222cb559d5c4a551b4fdd44926490b33e1
xiaobaifeng <835277213@qq.com>
2019-10-15 13:31:07 +0800

    feat: 添加日志中间件;
    feat: 添加log中间件(logger.js);
    
    feat: 在日志中间件内添加全局错误状态处理规则;
    
    feat: 引入log中间件;
    
    feat: 每次响应的时候生成一条日志;
    
    chore: update .gitignore;

--- middleware/index.js
+++ middleware/index.js
@@ -2,9 +2,22 @@ const path = require('path')
 const bodyParser = require('koa-bodyparser')
 const nunjucks = require('koa-nunjucks-2')
 const staticFiles = require('koa-static')
+const ip = require('ip')
 const miSend = require('./mi-send')
+// 引入日志中间件
+const miLog = require('./mi-log')
 
 module.exports = (app) => {
+    // 注册中间件
+    // 将配置中间件的参数在注册中间件时作为参数传入
+    app.use(miLog({
+        env: app.env,  // koa 提供的环境变量
+        projectName: 'koa2-tutorial',
+        appLogLevel: 'debug',
+        dir: 'logs',
+        serverIp: ip.address()
+    }))
+
     // 指定 public目录为静态资源目录，用来存放 js css images 等
     app.use(staticFiles(path.resolve(__dirname, "../public")))
 
