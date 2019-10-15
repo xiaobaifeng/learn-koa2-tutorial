92817755627e377d8202faf88ac3cc43ceb4d27e
xiaobaifeng <835277213@qq.com>
2019-10-15 13:47:58 +0800

    ci: 移除开发依赖包address;minor: 替换listener.js中获取本地ip的包;

--- listener.js
+++ listener.js
@@ -1,10 +1,10 @@
-const address = require('address')
+const ip = require('ip')
 const opn = require('opn')
 
 
 module.exports = (app) => {
     app.listen(3000, () => {
-        let localhost = address.ip();
+        let localhost = ip.address();
         let url = 'http://' + (localhost || 'localhost') + ':' + '3000';
         console.log('server is running at ', '\033[36;4m' + url + '\033[0m');
         opn(url);
