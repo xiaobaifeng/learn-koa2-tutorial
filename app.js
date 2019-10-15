bb071ff3112dacb4ddddb30dc905c65504bb547e
xiaobaifeng <835277213@qq.com>
2019-10-15 10:16:31 +0800

    feat: 在app.js中引入koa-nunjucks-2已经对应的例子(/user);

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const Koa = require('koa')
+const path = require('path')
 const bodyParser = require('koa-bodyparser')
-const app = new Koa()
+const nunjucks = require('koa-nunjucks-2')
 
+const app = new Koa()
 const router = require('./router')
 const listener = require('./listener')
+
+app.use(nunjucks({
+    ext: 'html',
+    path: path.join(__dirname, 'views'),// 指定视图目录
+    nunjucksConfig: {
+        trimBlocks: true // 开启转义 防Xss
+    }
+}));
 
 app.use(bodyParser())
 
--- controller/home.js
+++ controller/home.js
@@ -12,16 +12,9 @@ module.exports = {
         ctx.response.body = '<h1>HOME page /:id/:name</h1>'
     },
     login: async(ctx, next) => {
-        ctx.response.body =
-        `
-        <form action="/user/register" method="post">
-            <input name="name" type="text" placeholder="请输入用户名：ikcamp"/> 
-            <br/>
-            <input name="password" type="text" placeholder="请输入密码：123456"/>
-            <br/> 
-            <button>GoGoGo</button>
-        </form>
-        `
+        await ctx.render('home/login',{
+            btnName: 'GoGoGo'
+        })
     },
     register: async(ctx, next) => {
         let {
