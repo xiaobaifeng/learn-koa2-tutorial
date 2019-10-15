69a7e79ce2cbc908656cf99de4f694b69e7c0b43
xiaobaifeng <835277213@qq.com>
2019-10-15 13:40:36 +0800

    feat: 每次响应的时候生成一条日志;

--- middleware/mi-send/index.js
+++ middleware/mi-send/index.js
@@ -5,6 +5,8 @@ module.exports = () => {
     }
     return async (ctx, next) => {
         ctx.send = render.bind(ctx)
+        // 调用ctx上的log方法下的error方法打印日志
+        ctx.log.error('ikcamp');
         await next()
     }
 }
