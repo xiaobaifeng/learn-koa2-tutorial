8052940338d1c9706645ecb033319aa2e313b65f
xiaobaifeng <835277213@qq.com>
2019-10-15 11:35:11 +0800

    refactor: 抽离middleware到单独目录去维护;
    refactor: 抽离middleware到单独目录去维护;

--- app.js
+++ app.js
@@ -1,25 +1,10 @@
 const Koa = require('koa')
-const path = require('path')
-const bodyParser = require('koa-bodyparser')
-const nunjucks = require('koa-nunjucks-2')
-const staticFiles = require('koa-static')
 
 const app = new Koa()
+const middleware = require('./middleware')
 const router = require('./router')
 const listener = require('./listener')
 
-// 指定 public目录为静态资源目录，用来存放 js css images 等
-app.use(staticFiles(path.resolve(__dirname, "./public")))
-
-app.use(nunjucks({
-    ext: 'html',
-    path: path.join(__dirname, 'views'),// 指定视图目录
-    nunjucksConfig: {
-        trimBlocks: true // 开启转义 防Xss
-    }
-}));
-
-app.use(bodyParser())
-
+middleware(app)
 router(app)
 listener(app)
